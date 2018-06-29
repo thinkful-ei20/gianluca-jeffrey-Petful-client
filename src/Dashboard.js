6fc1bb6e80958c3e6106dab32cea81a3a2bfbb33
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-06-29 12:33:00 -0500

    Dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,2 +1,58 @@
 import React from 'react';
 
+export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onAdopt() {
+    console.log(`'onAdopt' ran`);
+  }
+
+  render() {
+    return (
+      <div>
+        <section>
+          <header>
+            <h2>{this.props.catToAdopt.name}</h2>
+            <img src={this.props.catToAdopt.imageURL} alt={this.props.catToAdopt.imageDescription} />
+          </header>
+          <main>
+            <dl>
+              <dt>Sex</dt>
+              <dd>{this.props.catToAdopt.sex}</dd>
+              <dt>Age</dt>
+              <dd>{this.props.catToAdopt.age}</dd>
+              <dt>Breed</dt>
+              <dd>{this.props.catToAdopt.breed}</dd>
+              <dt>Story</dt>
+              <dd>{this.props.catToAdopt.story}</dd>
+            </dl>
+            <button onClick={this.onAdopt}>Adopt</button>
+          </main>
+        </section>
+        <section>
+          <header>
+            <h2>{this.props.dogToAdopt.name}</h2>
+            <img src={this.props.dogToAdopt.imageURL} alt={this.props.dogToAdopt.imageDescription} />
+          </header>
+          <main>
+            <dl>
+              <dt>Sex</dt>
+              <dd>{this.props.dogToAdopt.sex}</dd>
+              <dt>Age</dt>
+              <dd>{this.props.dogToAdopt.age}</dd>
+              <dt>Breed</dt>
+              <dd>{this.props.dogToAdopt.breed}</dd>
+              <dt>Story</dt>
+              <dd>{this.props.dogToAdopt.story}</dd>
+            </dl>
+            <button onClick={this.onAdopt}>Adopt</button>
+          </main>
+        </section>
+      </div >
+    );
+
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Dashboard from './Dashboard';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let cat = {
+  imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+  imageDescription: 'Orange Bengal cat with black stripes lounging on concrete.',
+  name: 'Jim',
+  sex: 'Male',
+  age: 10,
+  breed: 'Bengal',
+  story: 'Lazy Cat, No body cares about him, :('
+};
+
+let dog = {
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Chunky',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'He has an eating disorder'
+};
+
+ReactDOM.render(<Dashboard catToAdopt={cat} dogToAdopt={dog} />, document.getElementById('root'));
 registerServiceWorker();
